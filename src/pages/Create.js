c2762f45929c137a2c230f73c810a3b6733d2e27
Asya1398 <asi.hakobyan@gmail.com>
2021-11-02 07:40:49 -0700

    added the button

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,19 @@
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
+// import AcUnitIcon from '@mui/icons-material/AcUnit';
+// import SendIcon from '@mui/icons-material/Send';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import {makeStyles}  from '@mui/styles';
+
+const useStyle=makeStyles({
+    btn:{
+        fontsize:60,
+        backgroundColor:'violet'
+    }
+})
 function Create() {
+    const classes = useStyle()
     return (
         <Container>
             <Typography
@@ -13,15 +25,24 @@ function Create() {
                 Create a new Note
             </Typography>
             <Button
-                onClick={()=>{
+                className={classes.btn}
+                onClick={() => {
                     console.log("you clicked me")
                 }}
                 type='Submit'
                 color='secondary'
                 variant='contained'
+                // startIcon={<SendIcon/>}
+                endIcon={<KeyboardArrowRightIcon/>}
             >
                 Submit
             </Button>
+            <br/>
+            {/*{icons}*/}
+            {/*<AcUnitIcon/>*/}
+            {/*<AcUnitIcon*/}
+            {/*    color='secondary'*/}
+            {/*    fontSize='large'/>*/}
         </Container>
     )
 }
